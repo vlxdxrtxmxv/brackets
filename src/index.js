74b74959856200ba9e6489f7f40fb482a236991e
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-07-31 15:17:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let bracketsPair = {};  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0];    
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i]; 
+    if (openBrackets.includes(current)) {
+      if ((stack[stack.length - 1] === current) && (bracketsPair[current] === current)) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (bracketsPair[current] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
